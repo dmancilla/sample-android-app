834963e1cfe18f28347e08fe6b0f2c5b9b0dab74
David Mancilla <dmancilla@gmail.com>
2015-09-25 16:54:08 -0300

    Removed appCompat references modified travis.yaml to run tests

--- app/src/main/groovy/cl/dman/android/sample123/MainActivity.groovy
+++ app/src/main/groovy/cl/dman/android/sample123/MainActivity.groovy
@@ -1,13 +1,11 @@
 package cl.dman.android.sample123
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import groovy.transform.CompileStatic
 
 @CompileStatic
-class MainActivity extends AppCompatActivity {
+class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
