6322262f6b732973f73c6d7144477e555d16659d
David Mancilla <dmancilla@gmail.com>
2015-09-25 16:17:12 -0300

    Removed methods from MainActivity Updated build tools version to 23.0.1 Updated com.android.tools.build:gradle to 1.2.3

--- app/src/main/groovy/cl/dman/android/sample123/MainActivity.groovy
+++ app/src/main/groovy/cl/dman/android/sample123/MainActivity.groovy
@@ -15,24 +15,4 @@ class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main)
     }
 
-    @Override
-    boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu)
-        true
-    }
-
-    @Override
-    boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.itemId
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        super.onOptionsItemSelected(item)
-    }
 }
